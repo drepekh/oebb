97e976925c7864b0429912f0ed0d0e312ffde50e
arahorn28 <drepekh@gmail.com>
2018-04-09 03:23:17 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='oebb',
+      version='1.0.0',
+      description='Python API client for Austrian Federal Railways',
+      url='https://github.com/arahorn28/oebb',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6'
+      ],
+      install_requires=['requests'])
